350f1a1b6387ceef51f84331c5af97f4a1785b8f
Shawn Sutherland <shawnsutherland@hotmail.com>
2022-08-17 15:18:50 +0200

    Added action methods.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,46 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant) if @restaurant.save
+  end
+
+  # def edit
+  #   @task = Task.find(params[:id])
+  # end
+
+  # def update
+  #   @task = Task.find(params[:id])
+  #   @task.update(task_params)
+
+  #   redirect_to task_path(@task)
+  # end
+
+  # def destroy
+  #   @task = Task.find(params[:id])
+  #   @task.destroy
+  #   # No need for app/views/restaurants/destroy.html.erb
+  #   redirect_to tasks_path, status: :see_other
+  # end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Clearing the db..."
+Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+  restaurant.save!
+end
+
+resaurants = Restaurant.all
+
+resaurants.each do |restaurant|
+  review = Review.new(
+    rating: rand(1..5),
+    content: Faker::Restaurant.review,
+    restaurant_id: restaurant.id
+  )
+  review.save!
+end
+
+puts "Done!"
